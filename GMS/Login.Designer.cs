d1f32ca509e55bc4bda7583937087880e4409e33
Andrea Nicolas <24-56465-1@student.aiub.edu>
2026-01-02 16:39:43 +0600

    Create all necessary tables for GMS database

--- GMS/Login.Designer.cs
+++ GMS/Login.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace GMS
 {
-    partial class Form1
+    partial class Login
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,20 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1904, 1041);
+            this.Name = "Login";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Login_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
     }
 }
-
