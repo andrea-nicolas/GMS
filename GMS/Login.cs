1d7eb1ced7bd50793dabf7621aca3aafb0f333f2
Andrea Nicolas <24-56465-1@student.aiub.edu>
2026-01-03 21:20:07 +0600

    Set up foreign keys

--- GMS/Login.cs
+++ GMS/Login.cs
@@ -21,5 +21,15 @@ namespace GMS
         {
 
         }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
